db2e339b22fea71c3ab72144f77584341a3c0ce8
Shu Ding <ds303077135@icloud.com>
2020-06-24 00:54:45 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,8 +22,8 @@ export async function unstable_getStaticProps() {
 export default ({ post }) => {
   return (
     <article>
-      <h1>Shu Ding</h1>
-      <Header title="Shu Ding" />
+      <h1>Chu Ding</h1>
+      <Header title="Chu Ding" />
       <Content blocks={post.content || []}/>
     </article>
   )
